52b9ef084a1bc1371b5d9ff65afb84c81d2e2777
James Koh <huisoonkoh@gmail.com>
2024-11-10 15:45:15 +0800

    Initial commit

---
+++ src/main/java/com/jameshskoh/oauth2_poc_svc/Oauth2PocSvcApplication.java
@@ -0,0 +1,13 @@
+package com.jameshskoh.oauth2_poc_svc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2PocSvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth2PocSvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jameshskoh/oauth2_poc_svc/Oauth2PocSvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jameshskoh.oauth2_poc_svc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2PocSvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
